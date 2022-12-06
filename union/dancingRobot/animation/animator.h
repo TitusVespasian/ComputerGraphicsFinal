2d72c152b368c98492c11f36a2bdab377e046034
nono <97236799+Peanut-Puff@users.noreply.github.com>
2022-12-07 03:44:20 +0800

    Add files via upload

--- union/dancingRobot/animation/animator.h
+++ union/dancingRobot/animation/animator.h
@@ -22,15 +22,10 @@ public:
 			m_FinalBoneMatrices.push_back(glm::mat4(1.0f));
 	}
 
-	void UpdateAnimation(float dt)
+	void UpdateAnimation()
 	{
-		m_DeltaTime = dt;
 		if (m_CurrentAnimation)
-		{
-			m_CurrentTime += m_CurrentAnimation->GetTicksPerSecond() * dt;
-			m_CurrentTime = fmod(m_CurrentTime, m_CurrentAnimation->GetDuration());
 			CalculateBoneTransform(&m_CurrentAnimation->GetRootNode(), glm::mat4(1.0f));
-		}
 	}
 
 	void PlayAnimation(Animation* pAnimation)
@@ -71,7 +66,13 @@ public:
 		return m_FinalBoneMatrices;
 	}
 
-	float getCurrentTime() {
+	float getCurrentTime(float dt) {
+		m_DeltaTime = dt;
+		if (m_CurrentAnimation)
+		{
+			m_CurrentTime += m_CurrentAnimation->GetTicksPerSecond() * dt;
+			m_CurrentTime = fmod(m_CurrentTime, m_CurrentAnimation->GetDuration());
+		}
 		return m_CurrentTime;
 	}
 
